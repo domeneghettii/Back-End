dab0ebcf893c66a45c847310e54e6438a172eaa4
unknown <luanaa.domeneghettii@gmail.com>
2025-04-01 08:54:13 -0300

    feat; criação das pastas e arquivos.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const userRoutes = require("./src/routes/userRoutes");
+//const wizardRoutes = require("./src/routes/wizardRoutes");
+const houseRoutes = require("./src/routes/houseRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", userRoutes);
+//app.use("/api/wizards", wizardRoutes);
+app.use("/api/houses", houseRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
-
