ac11b74d6fe78f51d3f10dd5b66d3850c321de2c
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-04-22 08:21:59 -0300

    feat: update reportRouses.js

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
 const reportController = require("./../controllers/reportController");
 const apiKeyMiddleware = require("./../config/apiKey")
 
 router.use(apiKeyMiddleware);
+//Rota para gerar CSV
 router.get("/report/csv", reportController.exportWizardCSV);
+
+//Rota para o PDF
 router.get("/report/pdf", reportController.exportWizardPDF);
+
+
+
+router.use(apiKeyMiddleware);
+
 
 module.exports = router;
