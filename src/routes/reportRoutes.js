b0004cf67eda7df3b966129434ac2642f7af26eb
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-04-22 08:17:44 -0300

    feat:  Create apiKey.js

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const reportController = require("./../controllers/reportController");
+const apiKeyMiddleware = require("./../config/apiKey")
 
-//Rota para gerar CSV
+router.use(apiKeyMiddleware);
 router.get("/report/csv", reportController.exportWizardCSV);
-
-//Rota para o PDF
 router.get("/report/pdf", reportController.exportWizardPDF);
-
 
 module.exports = router;
